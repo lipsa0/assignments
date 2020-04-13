8482f263eda3c9cf160cda2a029fbef8c4cb1cdf
lipsa0 <63600510+lipsa0@users.noreply.github.com>
2020-04-13 21:30:52 +0530

    Add files via upload

---
+++ assignment4.php
@@ -0,0 +1,6 @@
+<?php
+$word=array("condence"=>"more concentrated","blackout"=>"switching off","aspire"=>"strong desire" );
+echo "condence-->.$word[condence]";echo"<br>";
+echo "blackout-->.$word[blackout]";echo"<br>";
+echo "aspire-->.$word[aspire]";
+?>
